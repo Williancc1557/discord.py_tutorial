7a3cd5bb71410b06339bb6cce01ff51dd47c9845
Willian Cavalcanti <74304047+Williancc1557@users.noreply.github.com>
2021-04-01 16:06:36 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -7,6 +7,13 @@ bot = discord.Bot(command_prefix='w!')
 @bot.command()
 async def teste(ctx):
     await ctx.send('olá mundo')
+    
+@bot.command()
+async def embed(ctx):
+    msg = discord.Embed(title='Olá mundo',
+                        description=f'essa é a nossa descrição! executada pelo(a) {ctx.author}',
+                        color=0x00FF00)
+    await ctx.send(embed=msg)
 
 
 
